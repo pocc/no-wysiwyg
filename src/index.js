5c39d14c530b132aff87a326a66a0f738539d176
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-11-23 08:16:02 +0700

    Minify index.js code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 setTimeout(()=>{
-  const redux = slackDebug[slackDebug.activeTeamId].redux;
+  const {redux} = slackDebug.activeTeam;
   const {wysiwyg_composer, wysiwyg_composer_ios, wysiwyg_composer_webapp, ...payload} = redux.getState().experiments;
-  redux.dispatch({ type: '[19] Bulk add experiment assignments to redux', payload });
+  redux.dispatch({type: '[19] Bulk add experiment assignments to redux', payload});
 }, 5000)
